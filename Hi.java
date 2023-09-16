409a23d7e62598ce4a63c71f7444188bf5391be3
Le Van Hung <hungneit@gmail.com>
2023-09-16 16:52:57 +0700

    Create Hi.java

---
+++ Hi.java
@@ -0,0 +1,8 @@
+public class Hi {
+    public static void main(String[] args) {
+        System.out.print("Hi, "+args[2]+", "+args[1]+" and "+args[0]);
+        System.out.print(args[0]);
+        System.out.println(". How are you?");
+    }
+}
+
